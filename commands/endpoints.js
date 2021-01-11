834f04899e200fa248331ee234f02621d221474d
Trapss <duttyhef@gmail.com>
2021-01-11 08:54:45 -0600

    added ping command, endpoints command (todo: endpoints embed is ugly make it better)

---
+++ commands/endpoints.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'endpoints',
+    description: 'List of the shiro.gg api endpoints',
+    category: "api",
+    aliases: [],
+    execute(client, msg) {
+
+        const { MessageEmbed } = require('discord.js')
+
+        const embed = new MessageEmbed()
+        .setColor('36393E')
+        .setFooter('https://shiro.gg/api')
+        .addField('SFW /images Endpoints', 'avatars\nblush\nhug\nkiss\nneko\nnom\npat\npoke\npout\nslap\nsmug\ntickle\nwallpapers')
+        .addField('NSFW /images Endpoints', '/nsfw/bondage\n/nsfw/hentai\n/nsfw/thighs')
+
+        msg.channel.send(embed)
+
+    }
+}
---
+++ commands/ping.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: 'ping',
+    description: 'return ping to the apis used by the bot',
+    category: "util",
+    aliases: [],
+    execute(client, msg) {
+        const domainPing = require("domain-ping")
+        const moment = require('moment')
+
+        const { MessageEmbed } = require('discord.js')
+
+        const embed = new MessageEmbed()
+        .setColor('36393E')
+        .setFooter(moment.utc(client.readyAt).format('LLLL') + ' UTC')
+
+        domainPing('shiro.gg')
+            .then((res) => {
+                embed.addField("Discord API", `${Math.floor(client.ws.ping)}ms`, true)
+                embed.addField("Shiro.gg API", `${Math.floor(res.ping_time)}ms`, true)
+                msg.channel.send(embed)
+            })
+            .catch((error) => {
+                console.error(error)
+                embed.addField("Discord API", `${Math.floor(client.ws.ping)}ms`, true)
+                embed.addField("Shiro.gg API", `Unable to connect to Shiro.gg`, false)
+                msg.channel.send(embed)
+            })
+    }
+}
